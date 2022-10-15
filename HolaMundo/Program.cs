b749ab3eafad8d6248ea5ae863e06113ea829ecc
Huntress <quiqueze@gmail.com>
2022-10-15 16:52:37 -0500

    Practica con variables numericas

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -10,8 +10,31 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!");
-            Console.WriteLine("Hola Alquien!");
+            //Console.WriteLine("Hola Mundo!");
+            //Console.WriteLine("Hola Alquien!");
+            //Console.Read();
+
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1 + num2;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(sum);
+            Console.WriteLine("La suma de " + num1 + " mas " + num2 + " es igual a " + sum);
+
+            double d1 = 3.2;
+            double d2 = 1.3;
+            double sumd = d1 + d2;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(sumd);
+            Console.WriteLine("La suma de " + d1 + " mas " + d2 + " es igual a " + sumd);
+
+            double sumx = d1 + num2;
+            Console.WriteLine("La suma de " + d1 + " mas " + num2 + " es igual a " + sumx);
+
             Console.Read();
         }
     }
